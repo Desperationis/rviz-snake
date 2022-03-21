b073eb9f550df34120ec0f91e4ff4cbb6744bca4
Desperationis <smarttdiego@gmail.com>
2022-03-21 12:39:02 -0700

    Add Demo of fruit
    There is now code in place to place fruits on the board and eat them;
    Respawing them is TBD

--- src/snake_publisher/src/main.cpp
+++ src/snake_publisher/src/main.cpp
@@ -40,10 +40,32 @@ public:
 		}
 	}
 
+	/**
+	 * Determine whether a point is within the bounds of [0, side_length).
+	*/ 
+	bool InBounds(int x, int y) {
+		return x < sideLength && x >= 0 && 
+			y < sideLength && y >= 0;
+
+	}
+
 	void ReserveSnake(int x, int y) {
-		if (x < sideLength && y < sideLength) {
+		if (this->InBounds(x, y)) {
 			gridElements[y][x] = SNAKE;
 		}
+	}
+
+	void ReserveFruit(int x, int y) {
+		if (this->InBounds(x, y)) {
+			gridElements[y][x] = FRUIT;
+		}
+	}
+
+	/**
+	 * Returns the side length of the grid.
+	*/ 
+	int GetSideLength() {
+		return sideLength;
 	}
 
 	void Draw(std::shared_ptr<MarkerPublisher> publisher) {
@@ -59,6 +81,7 @@ public:
 					cube.SetColor(0, 1, 0, 1);
 					break;
 				case FRUIT:
+					cube.SetColor(1, 0, 0, 1);
 					break;
 				};
 
@@ -71,11 +94,84 @@ public:
 	}
 };
 
+struct Fruit {
+	Fruit(int x, int y) {
+		p.x = x;
+		p.y = y;
+	}
+
+	geometry_msgs::msg::Point p;
+};
+
 class Snake {
 public:
 	enum DIRECTION {LEFT, RIGHT, UP, DOWN};
 
 	Snake(int x, int y) {
+		Respawn(x, y);
+	}
+
+	std::list<geometry_msgs::msg::Point> GetBody() {
+		return body;
+	}
+
+	void Update(SnakeGrid& snakeGrid, std::vector<Fruit>& fruits) {
+		if(!dead) {
+			geometry_msgs::msg::Point nextPoint;
+			nextPoint = body.front();
+			switch(currentDirection) {
+			case LEFT:
+				nextPoint.x -= 1;
+				break;
+			case RIGHT:
+				nextPoint.x += 1;
+				break;
+			case UP:
+				nextPoint.y -= 1;
+				break;
+			case DOWN:
+				nextPoint.y += 1;
+				break;
+			};
+			
+			dead = WillDie(nextPoint.x, nextPoint.y, snakeGrid); // TODO; Pass in grid
+
+			if(!dead) {
+				body.push_front(nextPoint);
+
+				bool onFruit = false;
+				for(size_t i = 0; i < fruits.size(); i++) {
+					auto fruit = fruits[i];
+					if(fruit.p.x == nextPoint.x && fruit.p.y == nextPoint.y) {
+						onFruit = true;
+						fruits.erase(fruits.begin() + i);
+						break;
+					}
+				}
+
+				if(!onFruit) {
+					body.pop_back();
+				}
+			}
+		}
+	}
+
+	void SetDirection(DIRECTION dir) {
+		currentDirection = dir;
+	}
+
+	bool IsDead() {
+		return dead;
+	}
+
+	bool WillDie(int x, int y, SnakeGrid& snakeGrid) {
+		// TODO; Check bounds && self-intersection
+		return !snakeGrid.InBounds(x, y);
+	}
+
+	void Respawn(int x, int y) {
+		body.clear();
+		
 		geometry_msgs::msg::Point p;
 		p.x = x;
 		p.y = y;
@@ -84,38 +180,10 @@ public:
 		currentDirection = RIGHT;
 	}
 
-	std::list<geometry_msgs::msg::Point> GetBody() {
-		return body;
-	}
-
-	void Update() {
-		geometry_msgs::msg::Point nextPoint;
-		nextPoint = body.front();
-		switch(currentDirection) {
-		case LEFT:
-			nextPoint.x -= 1;
-			break;
-		case RIGHT:
-			nextPoint.x += 1;
-			break;
-		case UP:
-			nextPoint.y -= 1;
-			break;
-		case DOWN:
-			nextPoint.y += 1;
-			break;
-		};
-		body.push_front(nextPoint);
-		body.pop_back();
-	}
-
-	void SetDirection(DIRECTION dir) {
-		currentDirection = dir;
-	}
-
 private:
 	// First element is head, final element is tail.
 	std::list<geometry_msgs::msg::Point> body;
+	bool dead;
 	DIRECTION currentDirection;
 };
 
@@ -127,13 +195,19 @@ public:
 		this->renderTimer = this->create_wall_timer(80ms, std::bind(&GameNode::Loop, this));
 		this->inputTimer = this->create_wall_timer(10ms, std::bind(&GameNode::UserInput, this));
 		this->publisher = publisher;
+
+		fruits.push_back(Fruit(1,1));
 	}
 
 	void Loop() {
 		SnakeGrid grid(15);
-		snake.Update();
+		snake.Update(grid, fruits);
 		for(auto body : snake.GetBody()) {
 			grid.ReserveSnake(body.x, body.y);
+		}
+
+		for(auto fruit : fruits) {
+			grid.ReserveFruit(fruit.p.x, fruit.p.y);
 		}
 
 		grid.Draw(publisher);
@@ -157,6 +231,7 @@ private:
 	Snake snake;
 	rclcpp::TimerBase::SharedPtr renderTimer, inputTimer;
 	std::shared_ptr<MarkerPublisher> publisher;
+	std::vector<Fruit> fruits;
 };
 
 void TermCleanUp(int signum) {
