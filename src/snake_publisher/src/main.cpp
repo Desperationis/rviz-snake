3ae16eb39c94ccdb445c95bcad68cc8d3736ee20
Desperationis <smarttdiego@gmail.com>
2022-03-21 08:29:37 -0700

    Add "working" snake that moves
    Currently, there is a single block that gives the illusion of movement
    by using a doubly-linked list and redrawing on the grid. Since it is not
    ROS best practice to use a while loop, the whole game code resides on a
    single node that runs alongside the publisher node.

--- src/snake_publisher/src/main.cpp
+++ src/snake_publisher/src/main.cpp
@@ -1,10 +1,17 @@
 #include <memory>
+#include <thread>
+#include <functional>
+#include <chrono>
 #include <vector>
+#include <list>
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/executor.hpp"
 #include "visualization_msgs/msg/marker.hpp"
 #include "geometry_msgs/msg/point.hpp"
 #include "std_msgs/msg/color_rgba.hpp"
 #include "Markers.hpp"
+
+using namespace std::chrono_literals;
 
 /**
  * Wrapper for GridMarker so that origin is at (0,0), x-axis is positive to the
@@ -32,7 +39,9 @@ public:
 	}
 
 	void ReserveSnake(int x, int y) {
-		gridElements[y][x] = SNAKE;
+		if (x < sideLength && y < sideLength) {
+			gridElements[y][x] = SNAKE;
+		}
 	}
 
 	void Draw(std::shared_ptr<MarkerPublisher> publisher) {
@@ -60,14 +69,88 @@ public:
 	}
 };
 
+class Snake {
+public:
+	enum DIRECTION {LEFT, RIGHT, UP, DOWN};
+
+	Snake(int x, int y) {
+		geometry_msgs::msg::Point p;
+		p.x = x;
+		p.y = y;
+		body.push_front(p);
+
+		currentDirection = RIGHT;
+	}
+
+	std::list<geometry_msgs::msg::Point> GetBody() {
+		return body;
+	}
+
+	void Update() {
+		geometry_msgs::msg::Point nextPoint;
+		nextPoint = body.front();
+		switch(currentDirection) {
+		case LEFT:
+			nextPoint.x -= 1;
+			break;
+		case RIGHT:
+			nextPoint.x += 1;
+			break;
+		case UP:
+			nextPoint.y -= 1;
+			break;
+		case DOWN:
+			nextPoint.y += 1;
+			break;
+		};
+		body.push_front(nextPoint);
+		body.pop_back();
+	}
+
+	void SetDirection(DIRECTION dir) {
+		currentDirection = dir;
+	}
+
+private:
+	// First element is head, final element is tail.
+	std::list<geometry_msgs::msg::Point> body;
+	DIRECTION currentDirection;
+};
+
+
+class GameNode : public rclcpp::Node {
+public:
+	GameNode(std::shared_ptr<MarkerPublisher> publisher) : Node("game_runner"), snake(5,5) {
+		snake.SetDirection(Snake::RIGHT);
+		this->timer = this->create_wall_timer(1000ms, std::bind(&GameNode::Loop, this));
+		this->publisher = publisher;
+	}
+
+	void Loop() {
+		SnakeGrid grid(15);
+		for(auto body : snake.GetBody()) {
+			grid.ReserveSnake(body.x, body.y);
+		}
+
+		snake.Update();
+		grid.Draw(publisher);
+	}
+
+private:
+	Snake snake;
+	rclcpp::TimerBase::SharedPtr timer;
+	std::shared_ptr<MarkerPublisher> publisher;
+};
 
 int main(int argc, char** argv) {
 	rclcpp::init(argc, argv);
 	auto node = std::make_shared<MarkerPublisher>();
+	auto game = std::make_shared<GameNode>(node);
 
-	SnakeGrid grid(15);
-	grid.Draw(node);
+	auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+	executor->add_node(node);
+	executor->add_node(game);
+	executor->spin();
 
-	rclcpp::spin(node);
 	rclcpp::shutdown();
 }
