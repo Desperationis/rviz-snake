d8fc304415c7c9d2040c96f096d86943b6a93d60
Desperationis <smarttdiego@gmail.com>
2022-03-21 11:00:18 -0700

    Add ncurses demo to get input and exit gracefully
    This "demo" will not run the node until "q" is pressed on the prompt. If
    SIGINT is raised, the terminal will not be messed up.

--- src/snake_publisher/src/main.cpp
+++ src/snake_publisher/src/main.cpp
@@ -4,6 +4,7 @@
 #include <chrono>
 #include <vector>
 #include <ncurses.h>
+#include <csignal>
 #include <list>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/executor.hpp"
@@ -143,10 +144,37 @@ private:
 	std::shared_ptr<MarkerPublisher> publisher;
 };
 
+void TermCleanUp(int signum) {
+	if(signum == SIGINT) {
+		endwin();
+	}
+	rclcpp::shutdown();
+	abort();
+}
+
 int main(int argc, char** argv) {
+	std::signal(SIGINT, TermCleanUp);
+
 	rclcpp::init(argc, argv);
 	auto node = std::make_shared<MarkerPublisher>();
 	auto game = std::make_shared<GameNode>(node);
+
+	initscr();
+	noecho();
+    nodelay(stdscr, TRUE);
+	keypad(stdscr, TRUE);
+	cbreak();
+	clear();
+
+	printw("Hello wordl!!!");
+	refresh();
+	while(true) {
+		int c = getch();
+		if(c == 'q')
+			break;
+		std::this_thread::sleep_for(std::chrono::milliseconds(30));
+	}
+	endwin();
 
 	auto executor = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
 	executor->add_node(node);
